bf3f5e6ec61c245ca25e725c9de397ff2d80268b
takuya398 <takuya@example.com>
2026-01-30 20:00:25 +0900

    Add SQLite support for production

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b721330bd001cf9f"; }
+/******/ 		__webpack_require__.h = function() { return "b5de87143905dc43"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
